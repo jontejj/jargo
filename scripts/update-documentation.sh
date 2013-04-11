d7e3d96571ec5cb45ec78789bd7b5e1787071681
Jonatan Jönsson <jontejj@gmail.com>
2013-04-11 18:20:57 +0200

    Automatic javadoc updates for successful builds, Fixes #18

---
+++ scripts/update-documentation.sh
@@ -0,0 +1,24 @@
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo -e "Updating documentation at http://softhouse.github.io/jargo/\n"
+  #Generate documentation
+  mvn --quiet "javadoc:javadoc"
+  #TODO(jontejj): don't specify these manually
+  cp -R jargo/target/site/apidocs $HOME/javadoc/jargo
+  cp -R common-test/target/site/apidocs $HOME/javadoc/common-test
+  #Setup git
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis"
+  #clone gh-pages branch using encrypted gh-token
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages > /dev/null
+  cd gh-pages/javadoc
+  #Update files
+  rm -rf jargo/*
+  rm -rf common-test/*
+  cp -Rf $HOME/javadoc/ .
+  #Publish
+  git add --all .
+  git commit -m "Automatic javadoc update due to $TRAVIS_COMMIT"
+  git push -fq origin gh-pages > /dev/null
+  echo -e "Update of documentation complete\n"
+fi
